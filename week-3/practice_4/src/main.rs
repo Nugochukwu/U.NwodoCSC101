0e525dddc24118b55c98bf77cc2a5483404f5b10
ugochukwu <ugochukwu.nwodo@pau.edu.ng>
2022-10-28 17:27:04 +0100

    (My Fourth Commit)

---
+++ week-3/practice_4/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let special_character = '@'; //default
+    let alphabet:char = 'B';
+let surname = "Nwodo";
+let first_name = "Ugochukwu";
+let middle_name = "Alfred";
+
+
+println!(" ");
+println!("Special character: {}",special_character);
+println!("Alphabet: {}",alphabet);
+println!("Name: {} {} {}",surname, middle_name, first_name);
+
+}
