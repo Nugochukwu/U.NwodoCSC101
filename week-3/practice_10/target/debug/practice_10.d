bf8ebcf5da44c894d78e77e7326cc72233f78052
ugochukwu <ugochukwu.nwodo@pau.edu.ng>
2022-11-07 14:02:04 +0100

    (My Nineth Commit)

---
+++ week-3/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\U.NwodoCSC101\week-3\practice_10\target\debug\practice_10.exe: C:\Users\New\Documents\U.NwodoCSC101\week-3\practice_10\src\main.rs
---
+++ week-3/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+C:\Users\New\Documents\U.NwodoCSC101\week-3\practice_9\target\debug\practice_9.exe: C:\Users\New\Documents\U.NwodoCSC101\week-3\practice_9\src\main.rs
